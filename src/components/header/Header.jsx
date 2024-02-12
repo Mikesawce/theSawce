615237e5f50250c39118ed9ea06e5f85026afc0a
Michael Mote <michael.mote1993@gmail.com>
2024-02-12 15:23:17 -0600

    Update ContactMe and Header components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,12 +6,10 @@ const Header = () => {
 	const toggle = () => setIsToggled(!isToggled);
 
 	return (
-		<div className=' bg-zinc-100 min-h-20 flex shadow-2xl rounded'>
-			<div className='flex-1'>
-				<a className='font-calligraffitti text-black text-8xl ml-10 select-none'>
-					mikesawce
-				</a>
-			</div>
+		<div className='bg-zinc-100 min-h-20 flex shadow-2xl rounded'>
+			<a className='font-calligraffitti text-black text-8xl ml-10 select-none'>
+				mikesawce
+			</a>
 		</div>
 	);
 };
